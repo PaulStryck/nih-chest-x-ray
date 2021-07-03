2b58d89dc0d77e05d8bb468b4f00ddfef1f0a5ee
Stryck, Paul <paul.stryck@studium.uni-hamburg.de>
2021-07-03 12:44:33 +0200

    allow for arb size and rgb/grey conversion

--- convert.py
+++ convert.py
@@ -7,10 +7,16 @@ import pandas as pd
 import torchvision.transforms as transforms
 from PIL import Image
 
-transform = transforms.Compose([
+transform_rgb = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406],
                          std=[0.229, 0.224, 0.225])
+])
+
+transform_grey = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.456],
+                         std=[0.224])
 ])
 
 
@@ -57,18 +63,27 @@ def parse_csv(root: str, rel_label_file: str, rel_test_list: str, frac: float):
     return (_data_test, _data_train)
 
 
-def preload(df: pd.DataFrame, img_dir: str):
+def preload(df: pd.DataFrame, img_dir: str, size: int, rgb: bool):
     _labels = []
     _data   = []
+
+    # L for greyscale
+    conversion = 'RGB' if rgb else 'L'
 
     for idx, row in df.iterrows():
         print('Progress: {}'.format(idx))
         img_path = os.path.join(img_dir, row[0])
         img_path = glob.glob(img_path)
-        img = Image.open(img_path[0]).convert('RGB').resize((224, 224))
+        img = Image.open(img_path[0]).convert(conversion).resize((size, size))
+        if rgb:
+            transform = transform_rgb
+        else:
+            transform = transform_grey
+
         img_tr = transform(img)
 
         _data.append(np.array(img_tr))
+        print(conversion)
         _labels.append(row[2:18].values)
 
     return np.array(_labels), np.array(_data)
@@ -81,21 +96,26 @@ def main():
     parser.add_argument('--file-test',   type=str, default='test_list.txt')
     parser.add_argument('--frac',        type=float, default=1)
     parser.add_argument('--store-dir',   type=str, default='./')
+    parser.add_argument('--size',        type=int, default=224)
+    parser.add_argument('--rgb',         type=int, default=1)
     args = parser.parse_args()
+    args.rgb = bool(args.rgb)
 
-    test_data_file    = os.path.join(args.store_dir, 'test_224.npy')
-    test_target_file  = os.path.join(args.store_dir, 'test_tar_224.npy')
-    train_data_file   = os.path.join(args.store_dir, 'train_224.npy')
-    train_target_file = os.path.join(args.store_dir, 'train_tar_224.npy')
+    test_data_file    = os.path.join(args.store_dir, 'test_{}.npy'.format(args.size))
+    test_target_file  = os.path.join(args.store_dir, 'test_tar_{}.npy'.format(args.size))
+    train_data_file   = os.path.join(args.store_dir, 'train_{}.npy'.format(args.size))
+    train_target_file = os.path.join(args.store_dir, 'train_tar_{}.npy'.format(args.size))
 
     test, train = parse_csv(root           = args.data_dir,
                             rel_label_file = args.file_labels,
                             rel_test_list  = args.file_test,
                             frac           = args.frac)
-
+    print(args.rgb)
     np_test_labels, np_test_data = preload(test,
                                            os.path.join(args.data_dir,
-                                                        'images_*/images'))
+                                                        'images_*/images'),
+                                           args.size,
+                                           args.rgb)
     with open(test_data_file, 'wb') as f:
         np.save(f, np_test_data)
     del np_test_data
@@ -106,7 +126,9 @@ def main():
 
     np_train_labels, np_train_data = preload(train,
                                              os.path.join(args.data_dir,
-                                                          'images_*/images'))
+                                                          'images_*/images'),
+                                           args.size,
+                                           args.rgb)
     with open(train_data_file, 'wb') as f:
         np.save(f, np_train_data)
     del np_train_data
